88d730f8d5ef83e95f03c1080a994cbc2ee35a18
Terry Wells <terbos90803@gmail.com>
2018-03-07 20:31:40 -0800

    Multiple messages with BLE selector. Attempt at custom messages, but still not working.  Seems to be a problem with the BLE app on iPhone more than anything.

--- SkyWriter_101/SkyWriter_101.ino
+++ SkyWriter_101/SkyWriter_101.ino
@@ -10,27 +10,52 @@
 */
 
 #include <Adafruit_DotStar.h>
-#include <SPI.h>
-#include "CurieIMU.h"
+#include <CurieIMU.h>
+#include <CurieBLE.h>
 #include "FrameBuffer.h"
 #include "GLCDFont.h"
 #include "Tahoma19x21.h"
 
-const char defaultmessage[] = {
-  "4999  Momentum  4999"
+struct Message {
+  const char message[32];
+  const char fgcolors[32];
+  const char bgcolors[32];
+} const g_messages[] = {
+  {
+    "4999  Momentum  4999",
+    "88887799999999778888",
+    "00000000000000000000"
+  },
+  {
+    "4999  Momentum  4999",
+    "88887700000000778888",
+    "00000099999999000000"
+  },
+  {
+    "Go Go  Mo Mo",
+    "888888999999",
+    "000000000000"
+  }
 };
-const char fgcolors[] = {
-  "88887799999999778888"
-};
-const char bgcolors[] = {
-  "00000000000000000000"
-};
+const byte nMessages = sizeof(g_messages) / sizeof(g_messages[0]);
+char g_message[32] = "";
 
 const int NUMPIXELS = 72; // Number of LEDs in strip
 
 // Hardware SPI is a little faster, but must be wired to specific pins
 // (Arduino Uno and 101 = pin 11 for data, 13 for clock, other boards are different).
 Adafruit_DotStar strip(NUMPIXELS, DOTSTAR_BGR);
+
+
+//BLEPeripheral blePeripheral; // create peripheral instance
+// Selector service
+BLEService msgService("411acab3-23af-4a05-9bb7-35eaec0a7bb2"); // create service
+BLEUnsignedCharCharacteristic msgSelector("411acab3-23af-4a05-9bb7-35eaec0a7bb2", BLERead | BLEWrite);
+BLEDescriptor msgDesc("411acab3-23af-4a05-9bb7-35eaec0a7bb2", "Selector");
+// Custom service
+BLEService customService("7f4f6376-e0fa-45a3-ae89-47f162c30518"); // create service
+BLECharacteristic customMsg("7f4f6376-e0fa-45a3-ae89-47f162c30518", BLERead | BLEWrite, 20);
+BLEDescriptor customDesc("7f4f6376-e0fa-45a3-ae89-47f162c30518", "Custom");
 
 enum COLORS {
   BLACK = 0,
@@ -65,8 +90,20 @@ int msgMidPixel = 0;
 
 FrameBuffer framebuffer(strip, colormap);
 
-void renderMessage(const char *message, byte fgcolor, byte bgcolor)
+void renderMessage(byte msgSelection)
 {
+  const char *message = NULL, *fgcolors = NULL, *bgcolors = NULL;
+  if (msgSelection < nMessages)
+  {
+    message = g_messages[msgSelection].message;
+    fgcolors = g_messages[msgSelection].fgcolors;
+    bgcolors = g_messages[msgSelection].bgcolors;
+  }
+  else if (*g_message) {
+    message = g_message;
+    fgcolors = g_messages[0].fgcolors;
+    bgcolors = g_messages[0].bgcolors;
+  }
   msgLength = strlen(message);
   msgHeight = tahoma.getHeight();
   msgWidth = tahoma.render(message, fgcolors, bgcolors, framebuffer);
@@ -80,6 +117,44 @@ void renderMessage(const char *message, byte fgcolor, byte bgcolor)
   Serial.print("  msgWidth:");
   Serial.println(msgWidth);
   msgMidPixel = msgWidth / 2;
+}
+
+void BLEsetup()
+{
+  BLE.begin();
+  
+  // set the local name peripheral advertises.  Up to 8 chars.
+  BLE.setLocalName("SkyWrite");
+  BLE.setDeviceName("SkyWriter 101");
+  // set the UUID for the service this peripheral advertises
+  BLE.setAdvertisedService(msgService);
+  BLE.setAdvertisedService(customService);
+
+  // add service and characteristic
+  msgSelector.addDescriptor(msgDesc);
+  msgService.addCharacteristic(msgSelector);
+  BLE.addService(msgService);
+  customMsg.addDescriptor(customDesc);
+  customService.addCharacteristic(customMsg);
+  BLE.addService(customService);
+  
+  // assign event handlers for connected, disconnected to peripheral
+  BLE.setEventHandler(BLEConnected, blePeripheralConnectHandler);
+  BLE.setEventHandler(BLEDisconnected, blePeripheralDisconnectHandler);
+
+  // assign event handlers for characteristic
+  msgSelector.setEventHandler(BLEWritten, msgCharacteristicWritten);
+  // set an initial value for the characteristic
+  msgSelector.setValue(0);
+
+  // assign event handlers for characteristic
+  customMsg.setEventHandler(BLEWritten, customMsgWritten);
+  // set an initial value for the characteristic
+  customMsg.setValue("custom");
+
+  // advertise the service
+  BLE.advertise();
+  Serial.println(("Bluetooth device active, waiting for connections..."));
 }
 
 void setup() {
@@ -96,35 +171,11 @@ void setup() {
   // Valid values are: 125, 250, 500, 1000, 2000
   CurieIMU.setGyroRange(1000);
 
+  BLEsetup();
+
   // Render bitmap
-  renderMessage(defaultmessage, RED, BLACK);
+  renderMessage(0);
 }
-
-#if 0
-void selectColor(int c)
-{
-  color = c;
-}
-
-void chooseColor()
-{
-  unsigned long time = millis() / 4000;
-  switch (time % 3)
-  {
-    case 0:
-      selectColor(RED);
-      break;
-
-    case 1:
-      selectColor(GREEN);
-      break;
-
-    case 2:
-      selectColor(BLUE);
-      break;
-  }
-}
-#endif
 
 void loop() {
   static int fwdStartPixel = 0;
@@ -195,4 +246,38 @@ void loop() {
 
     framebuffer.displayColumn(-1);
   }
+
+  // poll peripheral
+  BLE.poll();
 }
+
+void blePeripheralConnectHandler(BLEDevice central) {
+  // central connected event handler
+  Serial.print("Connected event, central: ");
+  Serial.println(central.address());
+}
+
+void blePeripheralDisconnectHandler(BLEDevice central) {
+  // central disconnected event handler
+  Serial.print("Disconnected event, central: ");
+  Serial.println(central.address());
+}
+
+void msgCharacteristicWritten(BLEDevice central, BLECharacteristic characteristic) {
+  // central wrote new value to characteristic, update message
+  Serial.print("Characteristic event, written: ");
+
+  byte selection = msgSelector.value();
+  Serial.print("New message: ");
+  Serial.println(selection);
+  renderMessage(selection);
+}
+
+void customMsgWritten(BLEDevice central, BLECharacteristic characteristic) {
+  // central wrote new value to characteristic, update message
+  Serial.print("Characteristic event, written: ");
+
+  strcpy(g_message, (const char *)customMsg.value());
+  renderMessage(-1);
+}
+
