6b3f2c21e969091aaf0bedcc46ff2c7a68287fe5
Terry Wells <terbos90803@gmail.com>
2021-03-11 17:56:45 -0800

    Pickup math fixes for Heart

--- LightBox/Heart.h
+++ LightBox/Heart.h
@@ -3,7 +3,7 @@
 
 class Heart : public Raster
 {
-    const float step = M_PI/180;
+    const float step = M_PI/360;
     const uint32_t color = 0xff9afe;
     int frame = 0;
 
@@ -13,11 +13,11 @@ class Heart : public Raster
 
       for (float t = -M_PI; t <= M_PI; t += step) {
         float x1 = cos(t);
-        float y1 = sin(t);
-        float r = 30 * (sin(t)*sqrt(abs(cos(t))) / (sin(t) + 7/5) - 2*sin(t) + 2);
+        float y1 = -sin(t);
+        float r = 20 * (sin(t)*sqrt(abs(cos(t))) / (sin(t) + 7.0f/5.0f) - 2.0f*sin(t) + 2.0f);
         for (float ri = 0; ri <= r; ri += 1.0) {
           float x = x1 * ri + 50;
-          float y = y1 * ri + 50;
+          float y = y1 * ri + 20;
           getPixelSafe(x, y) = color;
         }
       }
