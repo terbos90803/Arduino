564c1f50c3b69b58ddd6d22d9a3ee491c054c2f9
Terry <terbos90803@gmail.com>
2021-05-23 21:38:57 -0700

    spin/pulse heart ; Add 5 random raindrops

--- LightBox/Heart.h
+++ LightBox/Heart.h
@@ -5,7 +5,9 @@ class Heart : public Raster
 {
     const float step = M_PI/360;
     const uint32_t color = 0xff9afe;
-    int frame = 0;
+    Matrix r1, r2;
+    float scale=1.0, grow=0.1;
+    float rot=0.0, spin=0.1;
 
   public:
     virtual void begin() override {
@@ -20,17 +22,28 @@ class Heart : public Raster
         float r = 20 * (st*sqrtf(abs(ct)) / (st + 7.0f/5.0f) - 2.0f*st + 2.0f);
         for (float ri = 0; ri <= r; ri += 1.0) {
           float x = x1 * ri + 50;
-          float y = y1 * ri + 20;
+          float y = y1 * ri + 17;
           getPixelSafe(x, y) = color;
         }
       }
+
+      r1.translate(-50,-50); // Translate the raster to be centered on the origin
+      r2.translate(50,50);   // Translate the scaled raster back to the display center
     }
 
     virtual void update() override {
-      frame = (frame + 1) % 100;
-      if (frame == 0)
-        m.reset();
-      m.scale(1.01);
+      if (scale < 1.0)
+        grow = 0.02;
+      else if (scale > 4.0)
+        grow = -0.02;
+      scale += grow;
+
+      rot += spin;
+      
+      m = r1;
+      m.scale(scale);
+      m.rotate(rot);
+      m.rmult(r2);
     }
 
 } heart;
--- LightBox/Raindrops.h
+++ LightBox/Raindrops.h
@@ -1,6 +1,55 @@
 class Raindrops : public Animation
 {
-    int frame = 0;
+    static constexpr int numDrops = 5;
+
+    class Drop
+    {
+        static constexpr float rate = 1.0;
+        float radius;
+        Pixel center;
+
+      public:
+        bool active {false};
+
+        void start() {
+          center.x = random(10, 90);
+          center.y = random(10, 90);
+          radius = 0;
+          active = true;
+        }
+
+        void grow() {
+          if (active) {
+            radius += rate;
+            if (radius > 200)
+              active = false;
+          }
+        }
+
+        uint32_t getColor(const Pixel &p) {
+          if (!active)
+            return 0;
+
+          // calc the distance from the pixel to the ring center
+          float dx = p.x - center.x;
+          float dy = p.y - center.y;
+          float dsq = dx * dx + dy * dy; // work in squared space to avoid a sqrt.  Also gives a nice fade effect.
+          //float d = sqrt(dsq);
+
+          // calc the size of the ring based on the size (gives animation)
+          float radsq = radius * radius;
+
+          // calc the distance from the pixel to the ring
+          float diff = abs(dsq - radsq); // difference in radii squared
+          //float diff = abs(d - radius); // difference in radii
+          float val = 255 - diff / 5; // The ring is full brightness.  brightness falls off further from the ring.
+          float b = val > 0 ? val : 0; // clip to avoid negative values
+
+          return strip.ColorHSV(43690, 255, b); // 43690 = 240*65536/360 = blue
+        }
+    } drops[numDrops];
+
+    int nextDrop = 0;
 
   public:
     virtual void begin() override {
@@ -8,27 +57,22 @@ class Raindrops : public Animation
     }
 
     virtual void update() override {
-      ++frame;
+      if (random(100) < 2) {
+        drops[nextDrop].start();
+        nextDrop = (nextDrop + 1) % numDrops;
+      }
+      for (Drop &d : drops) {
+        d.grow();
+      }
     }
 
     virtual uint32_t getColor(const Pixel &p) override {
-      // calc the distance from the pixel to the ring center
-      float dx = p.x - 50;
-      float dy = p.y - 50;
-      float dsq = dx * dx + dy * dy; // work in squared space to avoid a sqrt.  Also gives a nice fade effect.
-      //float d = sqrt(dsq);
-
-      // calc the size of the ring based on the frame number (gives animation)
-      float rad = (frame % 75);
-      float radsq = rad * rad;
-
-      // calc the distance from the pixel to the ring
-      float diff = abs(dsq - radsq); // difference in radii squared
-      //float diff = abs(d - rad); // difference in radii
-      float val = 255 - diff/5; // The ring is full brightness.  brightness falls off further from the ring.
-      float b = val > 0 ? val : 0; // clip to avoid negative values
-
-      return strip.ColorHSV(43690, 255, b); // 43690 = 240*65536/360 = blue
+      uint32_t c = 0;
+      for (Drop &d : drops) {
+        if (d.active)
+          c = blend(c, d.getColor(p));
+      }
+      return c;
     }
 
 } raindrops;
